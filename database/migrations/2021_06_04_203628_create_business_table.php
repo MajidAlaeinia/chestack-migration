053719cddef1952f97c66469358d8e5cf6b9f309
Majid Alaeinia <alaeinia.majid@gmail.com>
2021-06-11 01:36:08 +0430

    Use singular names for class names

--- database/migrations/2021_06_04_203628_create_business_table.php
+++ database/migrations/2021_06_04_203628_create_business_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBusinessesTable extends Migration
+class CreateBusinessTable extends Migration
 {
     /**
      * Run the migrations.
